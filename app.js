8df36ecd78ca0fef0160661349674d4346b14e44
Shantanu Jana <omni.7x@gmail.com>
2022-09-19 01:08:40 +0530

    updated app.js

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ const https = require("https");
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-    res.send("Server is working.");
+    res.sendFile(`${__dirname}/signup.html`);
 })
 
 app.listen(3000, () => {
