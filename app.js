6f886ca4d4755674986ce528cb757a9cabb2b832
Shantanu Jana <omni.7x@gmail.com>
2022-09-20 23:27:06 +0530

    updated app.js

--- app.js
+++ app.js
@@ -12,6 +12,14 @@ app.get("/", (req, res) => {
     res.sendFile(`${__dirname}/signup.html`);
 })
 
+
+app.post("/", (req, res) => {
+    let firstName = req.body.firstName;
+    let lastName = req.body.lastName;
+    let email = req.body.email;
+    res.send(`Posting request for ${firstName} ${lastName}.`);
+})
+
 app.listen(3000, () => {
     console.log("Server is running at port 3000.");
 })
